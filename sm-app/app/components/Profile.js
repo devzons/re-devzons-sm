1d301027bdb570a11d2713bda68f1f4fb2d725ae
devzons <devzons@gmail.com>
2020-12-14 06:07:27 -0800

    cleaning up useEffect

--- sm-app/app/components/Profile.js
+++ sm-app/app/components/Profile.js
@@ -16,15 +16,20 @@ function Profile() {
   })
 
   useEffect(() => {
+    const ourRequest = Axios.CancelToken.source()
+
     async function fetchData() {
       try {
-        const response = await Axios.post(`/profile/${username}`, { token: appState.user.token })
+        const response = await Axios.post(`/profile/${username}`, { token: appState.user.token, CancelToken: ourRequest.token })
         setProfileData(response.data)
       } catch (error) {
         console.log('there was a problem')
       }
     }
     fetchData()
+    return () => {
+      ourRequest.cancel()
+    }
   }, [])
 
   return (
